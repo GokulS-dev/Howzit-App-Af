7a1a94f2a456f1408d56b94d3654f3154cc9e696
GokulS-dev <gokulsakthivel7004@gmail.com>
2025-05-04 20:36:54 +0530

    commited

--- starconnectz-nest-master/src/example.service.ts
+++ starconnectz-nest-master/src/example.service.ts
@@ -4,7 +4,12 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class ExampleService {
   constructor(private readonly configService: ConfigService) {
-    const privateKey = fs.readFileSync(this.privateKeyPath, 'utf8');
-    console.log('Loaded PASETO_KEY:', pasetoKey);
+    const pasetoKey = this.configService.get<string>('PASETO_KEY');
+
+    if (!pasetoKey) {
+      throw new Error('❌ PASETO_KEY is not defined in .env');
+    }
+
+    console.log('✅ Loaded PASETO_KEY from .env:', pasetoKey);
   }
 }
